146cf63cf505ee4640aec7fbc94fb321bde7616e
yasaichi <yasaichi@users.noreply.github.com>
2016-12-17 14:23:57 +0900

    Implement generator: "rails g everett:install"

--- lib/generators/everett/install/install_generator.rb
+++ lib/generators/everett/install/install_generator.rb
@@ -1,3 +1,11 @@
-class InstallGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
+module Everett
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      def copy_initializer_file
+        template "everett.rb", "config/initializers/everett.rb"
+      end
+    end
+  end
 end
---
+++ lib/generators/everett/install/templates/everett.rb
@@ -0,0 +1,4 @@
+Everett.configure do |config|
+  # Activate observers that should always be running.
+  # config.observers = :my_observer
+end
